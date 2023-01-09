75e64dff3f77ee239800ee74c237d03d894fbf97
Rakshit <70335252+spuckhafte@users.noreply.github.com>
2023-01-09 08:41:56 +0530

    ::/fastr:: => jdb reading with require()

---
+++ fastr/functions/import/funcs.js
@@ -0,0 +1,76 @@
+const fs = require('fs');
+const fsP = require('fs/promises');
+const { __decryptMsg } = require('./encryption');
+
+function __checkIfDatabaseExists() {
+    let __folders = fs.readdirSync('./'); // get list of all files and folders
+    let __dbDirectory = __folders.find(_folder => _folder.startsWith('Db-')); // check if any starts with 'Db-'
+
+    if (__dbDirectory !== undefined) {
+        let __dbDirectoryPath = fs.statSync(process.cwd() + '/' + __dbDirectory); // get stats of the file/folder
+        if (__dbDirectoryPath.isDirectory()) { // it should be a directory
+            return __dbDirectory; // return folder name if it exists
+        } else {
+            return null; // return null if it is not a directory
+        }
+    }
+};
+
+
+async function __rGroupIsAuthentic(dbDir, rGroup) {
+    // in relational groups, all elements should have the same keys
+    // check if all elements have the same keys
+    let __files = await fsP.readdir('./' + dbDir + '/' + rGroup); // get list of all files
+    __files = __files.filter(_file => _file !== '__config.json'); // remove config file
+    // check if all files have the same keys
+    let __keys = []; // this array will contains arrays of keys of all elements
+    __files.forEach(async __file => {
+        let __fileObj = require(process.cwd() + '/' + dbDir + '/' + rGroup + '/' + __file);
+        let __fileKeys = Object.keys(__fileObj); // get all keys of the element
+        __keys.push(__fileKeys); // add the keys to the array
+    });
+
+    // check if all keys are the same
+    let __keysAreSame = true;;
+    for (let i = 0; i < __keys.length; i++) { // iterate through all key arrays
+        if (JSON.stringify(__keys[i]) !== JSON.stringify(__keys[0])) {
+            __keysAreSame = false;
+            break;
+        }
+    }
+    return __keysAreSame;
+};
+
+async function __exists(path) { // check if file exists
+    try {
+        await fsP.access(path); // this would raise error if the path does not exist
+        return true;
+    } catch (err) {
+        return false;
+    }
+};
+
+// get entry from a relational group by its moral
+function __getEntry(dbDir, group, element, moral) {
+    let elementObj = require(process.cwd() + '/' + dbDir + '/' + group + '/' + element + '.json')
+    // check if moral is in the element
+    let entries = Object.keys(elementObj) // get all entries in the element
+    let requiredEntry = null // this will be the required entry if it exists
+    entries.forEach(entry => {
+        if (__decryptMsg(elementObj[entry]) === moral) { // if the moral is in the entry
+            requiredEntry = entry // set the required entry
+        }
+    })
+
+    return requiredEntry
+};
+
+function greenConsole(text) {
+    console.log('\x1b[32m' + text + '\x1b[0m');
+};
+
+function yellowConsole(text) {
+    console.log('\x1b[33m' + text + '\x1b[0m');
+};
+
+module.exports = { __checkIfDatabaseExists, __rGroupIsAuthentic, __exists, greenConsole, yellowConsole, __getEntry };
